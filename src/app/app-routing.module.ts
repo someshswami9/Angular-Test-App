3993d6a88223bf5fb545e608b081b26855354f5d
Bamayya Swami <bamayya@truecopy.com>
2023-02-10 11:38:45 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+    pathMatch: 'full',
+    data: { title: 'Homepage' }
+  },
+  {
+    path: 'about-us',
+    component: AboutUsComponent,
+    data: { title: 'About Us' }
+  },
+  {
+    path: 'contact-us',
+    component: ContactUsComponent,
+    data: { title: 'Contact Us' }
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
